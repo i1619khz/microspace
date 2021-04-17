ed47bb78479ab39b8111237d6fa01ec6680864cf
1619kHz <1517262320@qq.com>
2021-04-17 14:23:35 +0800

    格式化代码

--- src/main/java/io/microspace/server/annotation/WebSocket.java
+++ src/main/java/io/microspace/server/annotation/WebSocket.java
@@ -36,5 +36,5 @@ import java.lang.annotation.Target;
 @Target({ElementType.TYPE})
 @Retention(RetentionPolicy.RUNTIME)
 public @interface WebSocket {
-  String value()  default "";
+  String value() default "";
 }
