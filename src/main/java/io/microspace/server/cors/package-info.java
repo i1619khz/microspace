36d40563598589c4847cb114eef8a407ef27a88c
1619khz <1517262320@qq.com>
2022-02-12 17:50:18 +0800

    cors支持

--- src/main/java/io/microspace/server/cors/package-info.java
+++ src/main/java/io/microspace/server/cors/package-info.java
@@ -21,30 +21,4 @@
  * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
  * SOFTWARE.
  */
-module microspace {
-    requires static com.google.common;
-    requires static micrometer.core;
-    requires static reflections8;
-    requires static jsr305;
-    requires static io.netty.transport;
-    requires static io.netty.transport.epoll;
-    requires static io.netty.common;
-    requires static io.netty.handler;
-    requires static io.netty.codec;
-    requires static io.netty.buffer;
-    requires static io.netty.codec.http;
-    requires static org.checkerframework.checker.qual;
-    requires static com.google.errorprone.annotations;
-    requires static com.fasterxml.jackson.core;
-    requires static banana;
-    requires static juel.api;
-    requires static juel.impl;
-    requires org.slf4j;
-
-    exports io.microspace.context;
-    exports io.microspace.expression;
-    exports io.microspace.inject;
-    exports io.microspace.internal;
-    exports io.microspace.server;
-    exports io.microspace.aop;
-}
+package io.microspace.server.cors;
